8bbb50459dc58cb95ec4eb14040e021a410c9fe3
Peter Wu <peter@lekensteyn.nl>
2015-10-20 15:03:44 +0200

    Convert print to logger
    Use the logging module instead of printing to stdout. This allows the
    program output to be written to stdout without being interleaved with
    messages.
    
    This is also a step to Python 3 compatibility.

--- parse8xp.py
+++ parse8xp.py
@@ -1,12 +1,16 @@
 # A simple script to convert TI83/84 program files (.8xp) to raw text and back.
 
 import dict_source
+import logging
 
 __author__ = "Spork Interactive"
 __version__ = "$Revision: 1.3 $"
 __date__ = "$Date: 2009/09/22 $"
 __copyright__ = "Copyright (c) 2009 Spork Interactive"
 __license__ = "GNU"
+
+_logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 def loadDict (dictType = "compile", options = dict_source.index()):
     ki=0 # ki is the key index
@@ -22,7 +26,7 @@ def loadDict (dictType = "compile", options = dict_source.index()):
             for li in m:
                 d[li[ki]] = li[vi]
         except AttributeError:
-            print "%s does not exist!" % item
+            _logger.error("%s does not exist!", item)
     for li in dict_source.base():
         #Get the base values
         d[li[ki]] = li[vi]
@@ -50,9 +54,9 @@ def decompile(source8xp, destination):
                 protect = readFile.read(1) == "\x05" and "not " or ""
                 name = readFile.read(8)
                 writeFile.write("%s\n%sprotected\n%s\n" % (name, protect, comment))
-                print "Loading %s..." % name
-                print "Program is %sprotected" % protect
-                print comment
+                _logger.info("Loading %s...", name)
+                _logger.info("Program is %sprotected", protect)
+                _logger.info(comment)
                 # Find data's end
                 readFile.seek(-3, 2)
                 fileLength = readFile.tell()
@@ -69,21 +73,21 @@ def decompile(source8xp, destination):
                         current = readFile.read(readLen)
                     if tDict.has_key(current):
                         writeFile.write(tDict[current])
-                        #print tDict[current]
+                        #_logger.debug(tDict[current])
                     else:
                         writeFile.write("&@%s" % current)
-                        print "character %s not found!" % hex(ord(current))
-                print "%s successfully decompiled as %s" % (source8xp, destination)
+                        _logger.warning("character %x not found!", ord(current))
+                _logger.info("%s successfully decompiled as %s", source8xp, destination)
             except IOError:
-                print "A file error occurred during translation"
+                _logger.error("A file error occurred during translation")
             finally:
                 writeFile.close()
         except IOError:
-            print "Error creating %s!" % destination
+            _logger.error("Error creating %s!", destination)
         finally:
             readFile.close()
     except IOError:
-        print "Error loading %s!" % source8xp
+        _logger.error("Error loading %s!", source8xp)
 
 def recompile(source, destination8xp):
     """Open a text file, compile it as an 8xp.
@@ -138,11 +142,11 @@ def recompile(source, destination8xp):
                             writeBuffer.append(current)
                         # B) This is an invalid escape sequence. Let's announce that.
                         else:
-                            print "%s is an invalid escape sequence on line %s" % (current, lineNum)
+                            _logger.warning("%s is an invalid escape sequence on line %s", current, lineNum)
                             writeBuffer.append("ERROR %s" % current)
                     else:
                         # A character is unreadable!
-                        print "%s not found on line &s" % (current, lineNum)
+                        _logger.warning("%s not found on line &s", current, lineNum)
                         writeBuffer.append("ERROR %s" % current)
                 programMeat="".join(writeBuffer)
                 # Comment Code
@@ -177,17 +181,17 @@ def recompile(source, destination8xp):
                 writeFile.write(programMeat)
                 # Checksum - sum of all characters in program
                 writeFile.write(twoByte(sum([ord(c) for c in programMeat])))
-                print "%s successfully compiled as %s" % (source, destination8xp)
+                _logger.info("%s successfully compiled as %s", source, destination8xp)
             except IOError:
-                print "A file error occurred during translation"
+                _logger.error("A file error occurred during translation")
             finally:
                 writeFile.close()
         except IOError:
-            print "Error creating %s!" % destination
+            _logger.error("Error creating %s!", destination)
         finally:
             readFile.close()
     except IOError:
-        print "Error loading %s!" % source8xp
+        _logger.error("Error loading %s!", source8xp)
 
 def spellcheck(filename):
     """This function will perform a spell check operation on the indicated file,
@@ -238,7 +242,7 @@ def spellcheck(filename):
                         writeBuffer.append((current, lineNum))
                     # B) This is an invalid escape sequence. Let's announce that.
                     else:
-                        print "%s is an invalid escape sequence on line %s" % (current, lineNum)
+                        _logger.warning("%s is an invalid escape sequence on line %s", current, lineNum)
                         writeBuffer.append((current, lineNum))
                 else:
                     # A character is unreadable!
@@ -254,8 +258,8 @@ def spellcheck(filename):
 ##                    lineNum += 1
 ##                if raw.has_key(currentraw) and len(current) <= len(raw[currentraw]) and (not tDict.has_key(current)) and (not raw[currentraw] == current):
 ##                    #This is the definition of a spelling error...
-##                    print "%s '%s'? found on line %s" % (current, raw[rawize(current)], lineNum)
-            print "File loaded. Analyzing..."
+##                    _logger.warning("%s '%s'? found on line %s", current, raw[rawize(current)], lineNum)
+            _logger.info("File loaded. Analyzing...")
             for item in writeBuffer:
                 if rawize(item[0]):
                     i = writeBuffer.index(item)
@@ -268,7 +272,7 @@ def spellcheck(filename):
                         current = "%s%s" % (current, writeBuffer[i][0])
                         i+=1
                         #full_list.append(current)
-                        #print current
+                        #_logger.debug(current)
                     # Step 2: Reduce the chunk
                     original = current
                     while current and not raw.has_key(current):
@@ -276,21 +280,21 @@ def spellcheck(filename):
                     # Step 3: Report.
                     if raw.has_key(rawize(current)) and original[:len(raw[rawize(current)])] != raw[rawize(current)] and not tDict.has_key(current):
                         # A spelling error is a word found misspelled
-                        print "%s (Line %4d): '%s'" % (original.replace("\n", " ").replace("\r", "").ljust(maxKeyLen)[:maxKeyLen], item[1], raw[rawize(current)])
+                        _logger.warning("%s (Line %4d): '%s'", original.replace("\n", " ").replace("\r", "").ljust(maxKeyLen)[:maxKeyLen], item[1], raw[rawize(current)])
         except IOError:
-            print "A file error occurred during translation"
+            _logger.error("A file error occurred during translation")
         finally:
             readFile.close()
     except IOError:
-        print "Error loading %s!" % filename
+        _logger.error("Error loading %s!", filename)
 
 def gethelp(command, results=20):
     """Print help with a command to terminal"""
     tDict = loadDict(dictType="help")
     if tDict.has_key(command):
-        print tDict[command] or "No help available"
+        print(tDict[command] or "No help available")
     else:
-        print "%s not found" % command
+        print("%s not found" % command)
         rawize = lambda s: "".join([c for c in s.lower() if ord(c) > 47])
         raw = rawize(command)
         partmatch = lambda s:command in s # Partial match function
@@ -301,9 +305,9 @@ def gethelp(command, results=20):
             if partmatch(k):
                 matches += 1
                 if matches <= results:
-                    print "  %s: %s" % (k, v or "No help available")
+                    print("  %s: %s" % (k, v or "No help available"))
         if matches > results:
-            print "  *** %s results omitted" % (matches - results)
+            print("  *** %s results omitted" % (matches - results))
 
 #if __name__ == "__main__":
 #def test():
